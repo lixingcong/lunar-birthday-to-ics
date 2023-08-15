8528618172756e7daaa0916a681b531698b4b2de
lixingcong <lixingcong@live.com>
2023-08-15 20:56:11 +0800

    simple version

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
 # 农历生日导出到ics日历文件
 
-依赖：
+## 依赖：
 
 - [ics-py](https://github.com/ics-py/ics-py)
 
-使用
+- [LunarCalendar](https://github.com/wolfhong/LunarCalendar)
+
+## 使用
 
 ```
-python3 main.py
+pip3 install ics
+pip3 install LunarCalendar
+
+# 生成从今年开始的未来50年的农历生日
+python3 main.py -i config.json -c 50
+
+# 或者直接导出到文件
+python3 main.py -i config.json -c 50 > /tmp/exported.ics
 ```
 
+注意[config.json](config-example.json)中的```birthday```字段为公历出生日期
+
+## 导入到Google日历
+
+1. 为确保不影响现有的日历，先在[Google日历](https://calendar.google.com)中创建一个单独的日历，如"我的农历生日"
+2. 设置该日历的"全天日程"默认提醒方式，如"提前3日发邮件+提前4小时弹出提醒"
+3. 将ics导入到该日历
+4. 若有误操作，可以直接删除该日历
+
+## 参考项目
+
+- [农历事件生成器lcal](https://github.com/hotoo/lcal)
--- main.py
+++ main.py
@@ -1,16 +1,97 @@
 #!/usr/bin/env python3
 
-from datetime import datetime, timedelta
-from ics import Calendar, Event, alarm, Attendee
+import argparse
+from datetime import datetime
+from ics import Calendar, Event
+from lunarcalendar import Converter, Solar, Lunar
+import json
+import os
+import sys
 
-c = Calendar()
-e = Event()
-e.name = "哈哈"
-e.description = "A meaningful description"
-e.begin = datetime.fromisoformat("2023-08-14T12:05:23+08:00")
-e.end = datetime.fromisoformat("2023-08-14T13:05:23+08:00")
-e.created = datetime.now()
-e.alarms.append(alarm.EmailAlarm(trigger=timedelta(hours=-2), subject='subject', body='body', recipients=[Attendee('x@x.com')]))
-c.events.add(e)
+# with open()
 
-print(c.serialize())
+# calendar = Calendar()
+
+
+# print(calendar.serialize())
+
+# Global vars
+KEY_PERSONS='persons'
+KEY_NAME = 'name'
+KEY_BIRTHDAY = 'birthday'
+KEY_BIRTHDAY_LUNAR = 'birthday_lunar'
+
+def die(err):
+    sys.exit(err)
+
+# Returns: [['name':'A', 'birthday':'2023-09-09'], ['name':'A', 'birthday':'2023-09-09']]
+# Returned birthday is lunar
+def json_to_persons(json_file_path):
+    ret = []
+    with open(json_file_path, 'r') as f:
+        json_object = json.load(f)
+        if KEY_PERSONS in json_object:
+            for person in json_object[KEY_PERSONS]:
+                if KEY_NAME not in person:
+                    die(f'missing key {KEY_NAME} in {json_file_path}')
+                
+                if KEY_BIRTHDAY not in person:
+                    die(f'missing key {KEY_BIRTHDAY} in {json_file_path}')
+                
+                birthday = datetime.strptime(person[KEY_BIRTHDAY], '%Y-%m-%d')
+                birthday_luna = Converter.Solar2Lunar(Solar(birthday.year, birthday.month, birthday.day))
+
+                person[KEY_BIRTHDAY] = birthday
+                person[KEY_BIRTHDAY_LUNAR] = birthday_luna
+                ret.append(person)                                    
+        else:
+            die(f'missing key {KEY_PERSONS} in {json_file_path}')
+    return ret
+
+if __name__ == "__main__":
+    script_file = os.path.basename(sys.argv[0])
+    parser = argparse.ArgumentParser(prog=script_file,
+                                     formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=80),
+                                     description='A simple script to generate chinese lunar calendar birthday')
+    parser.add_argument('-i', metavar='/path/to/config.json', help='The input config file', required=True)
+    parser.add_argument('-c', metavar='COUNT', help='The repeat times of the event, default value: 50', default=50, type=int)
+
+    args = vars(parser.parse_args())
+    # print(args)
+
+    json_file_path = args['i']
+    event_count = args['c']
+    
+    persons = json_to_persons(json_file_path)
+    # print(persons)
+
+    calendar = Calendar()
+    event_steps = list(range(event_count))
+    this_year = datetime.today().year
+    
+    for person in persons:
+        birthday_lunar= person[KEY_BIRTHDAY_LUNAR]
+        birthday_lunar_month = birthday_lunar.month
+        birthday_lunar_day = birthday_lunar.day
+        birthday_year = person[KEY_BIRTHDAY].year
+        name = person[KEY_NAME]
+        
+        for step in event_steps:
+            new_year = this_year + step
+            age = new_year - birthday_year
+            if age < 0:
+                continue
+            
+            e = Event()
+            e.name = f'{name}的农历{age}岁生日'
+            e.description = f'祝生日快乐，{birthday_year}年出生，又长大一岁'
+
+            new_birthday_lunar = Lunar(this_year, birthday_lunar_month, birthday_lunar_day, isleap=False)
+            new_birthday_solar = Converter.Lunar2Solar(new_birthday_lunar)
+            
+            e.begin = datetime(new_birthday_solar.year, new_birthday_solar.month, new_birthday_solar.day)
+            e.make_all_day()
+            e.created = datetime.now()
+            calendar.events.add(e)
+
+    print(calendar.serialize())
